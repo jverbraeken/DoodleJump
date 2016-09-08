f60c0040a050fcfdaf8c6842532f98051955f41c
Cornel <c.o.devroomen@student.tudelft.nl>
2016-09-08 13:44:44 +0200

    Added platform location

--- src/main/java/objects/blocks/Platform.java
+++ src/main/java/objects/blocks/Platform.java
@@ -6,11 +6,18 @@ import objects.GameObject;
  * Created by Nick on 7-9-2016.
  */
 public class Platform extends GameObject implements IPlatform {
+    private int xPos;
+    private int yPos;
 
+    /**
+     * Creat and initiate the Platform object
+     * @param x - the X position of the platform
+     * @param y - the y position of the platform
+     */
     public Platform(int x, int y) {
         super();
-        //xPos = x;
-        //yPos = y;
+        xPos = x;
+        yPos = y;
     }
 
     //TODO: change to use Graphics (swing?)
