22c90215580a7bf24e365155be6e7efbd241a158
Nick Winnubst <nwinnubst@gmail.com>
2016-09-21 18:24:57 +0200

    Kill screen and ISceneFactory checkstyle fixes

--- src/main/java/system/IServiceLocator.java
+++ src/main/java/system/IServiceLocator.java
@@ -68,7 +68,7 @@ public interface IServiceLocator {
     IRenderer getRenderer();
 
     ISpriteFactory getSpriteFactory();
-    
+
     ISceneFactory getSceneFactory();
 
     IPlatformFactory getPlatformFactory();
