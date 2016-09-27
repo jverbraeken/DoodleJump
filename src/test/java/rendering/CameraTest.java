df384c92f1f6b1dda9a0cb891d4c434ca15b66c6
Joost Verbraeken <j.verbraeken@hotmail.com>
2016-09-27 19:03:50 +0200

    Add tests for the camera

---
+++ src/test/java/rendering/CameraTest.java
@@ -0,0 +1,42 @@
+package rendering;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.lang.reflect.Field;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class CameraTest {
+    static ICamera camera;
+
+    @Before
+    public static void init() {
+        camera = new Camera();
+    }
+
+    @Test
+    public static void testGetYPos() throws NoSuchFieldException, IllegalAccessException {
+        Field field = camera.getClass().getDeclaredField("y");
+        field.setAccessible(true);
+
+        field.set(camera, 2d);
+        assertThat(camera.getYPos(), is(2d));
+
+        field.set(camera, 3d);
+        assertThat(camera.getYPos(), is(3d));
+    }
+
+    @Test
+    public static void testSetYPos() throws NoSuchFieldException, IllegalAccessException {
+        Field field = camera.getClass().getDeclaredField("y");
+        field.setAccessible(true);
+
+        camera.setYPos(2d);
+        assertThat(field.get(camera), is(2d));
+
+        camera.setYPos(3d);
+        assertThat(field.get(camera), is(3d));
+    }
+}
