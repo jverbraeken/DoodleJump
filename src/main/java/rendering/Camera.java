1a0e9649d7241df323fc7a8c486c1bcb43404cbe
Joost Verbraeken <j.verbraeken@hotmail.com>
2016-09-27 18:53:52 +0200

    Add javadoc for the camera classes

--- src/main/java/rendering/Camera.java
+++ src/main/java/rendering/Camera.java
@@ -1,18 +1,33 @@
 package rendering;
 
+/**
+ * A standard implementation of the {@link ICamera} interface.
+ */
 /* package */ final class Camera implements ICamera {
 
+    /**
+     * The y-coordinate of the camera. When the doodle jumps high enough, this value decreases.
+     */
     private static double y = 0d;
 
+    /**
+     * Prevent instantiation from outside the package.
+     */
     /* package */ Camera() {
 
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
-    public void setYPos(double y) {
+    public void setYPos(final double y) {
         Camera.y = y;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public double getYPos() {
         return Camera.y;
--- src/main/java/rendering/ICamera.java
+++ src/main/java/rendering/ICamera.java
@@ -1,9 +1,19 @@
 package rendering;
 
+/**
+ * Provides a public interface for camera implementations.
+ */
 public interface ICamera {
 
-    void setYPos(double y);
+    /**
+     * Sets the y-position of the camera to the value specified.
+     * @param y The new y-value of the camera
+     */
+    void setYPos(final double y);
 
+    /**
+     * @return The y-position of the camera
+     */
     double getYPos();
 
 }
