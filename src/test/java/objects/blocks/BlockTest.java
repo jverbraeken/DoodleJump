53727d1a05de5b0763dbfcf3e32b56760ad06a51
Michael Tran <m.tran@student.tudelft.nl>
2016-09-27 23:24:04 +0200

    Fix checkstyle bugs in BlockTest.

--- src/test/java/objects/blocks/BlockTest.java
+++ src/test/java/objects/blocks/BlockTest.java
@@ -24,13 +24,12 @@ import static org.powermock.api.mockito.PowerMockito.mock;
  */
 public class BlockTest {
 
-    IServiceLocator servicelocator;
-    IGameObject gameobject = Mockito.mock(AGameObject.class);
-    IJumpable jumpobject = Mockito.mock(IJumpable.class);
-    IBlock block;
-    IPlatform platform = mock(Platform.class);
-    Set<IGameObject> set = new HashSet<>();
-
+    private IServiceLocator servicelocator;
+    private IGameObject gameobject = Mockito.mock(AGameObject.class);
+    private IJumpable jumpobject = Mockito.mock(IJumpable.class);
+    private IBlock block;
+    private IPlatform platform = mock(Platform.class);
+    private Set<IGameObject> set = new HashSet<>();
 
 
     @Before
@@ -40,9 +39,9 @@ public class BlockTest {
 
     /**
      * Tests if the getElements method has the set that is given as parameter to the constructor.
-     * @throws Exception
-     *                  throws an exception when the private constructor can not be called or when an exception is thrown
-     *                  in the constructor.
+     *
+     * @throws Exception throws an exception when the private constructor can not be called or when an exception is thrown
+     *                   in the constructor.
      */
     @Test
     public void testGetElements() throws Exception {
@@ -53,11 +52,11 @@ public class BlockTest {
     }
 
     /**
-     *  Test if the jumpobject is present in the set of IGameObjects after being added to that set.
-     *  It also tests if the topJumpable is indeed the jumpobject that has been added.
+     * Test if the jumpobject is present in the set of IGameObjects after being added to that set.
+     * It also tests if the topJumpable is indeed the jumpobject that has been added.
      */
     @Test
-    public void testGetTopJumpable() throws Exception{
+    public void testGetTopJumpable() throws Exception {
 
         block = Whitebox.invokeConstructor(Block.class, servicelocator, set, jumpobject);
         assertEquals(jumpobject, block.getTopJumpable());
@@ -66,9 +65,9 @@ public class BlockTest {
 
     /**
      * Tests that for each element in the set of IGameObjects the render in their own class is called.
-     * @throws Exception
-     *                  throws an exception when the private constructor can not be called or when an exception is thrown
-     *                  in the constructor.
+     *
+     * @throws Exception throws an exception when the private constructor can not be called or when an exception is thrown
+     *                   in the constructor.
      */
     @Test
     public void testRender() throws Exception {
