0e97f4294a92d1fd611c5b4748c98646c2750b48
Cornel <c.o.devroomen@student.tudelft.nl>
2016-09-28 19:30:32 +0200

    Fixed mistakes

--- src/test/java/math/CalcTest.java
+++ src/test/java/math/CalcTest.java
@@ -1,6 +1,5 @@
 package math;
 
-
 import org.junit.Before;
 import org.junit.Test;
 import static org.powermock.api.mockito.PowerMockito.mock;
@@ -12,53 +11,53 @@ import system.IServiceLocator;
 
 public class CalcTest {
 
-    private IServiceLocator sl;
-    private ICalc c;
+    private IServiceLocator sL;
+    private ICalc calc;
 
     @Before
-    public void init() throws Exception{
+    public void init() throws Exception {
         IServiceLocator sl = mock(IServiceLocator.class);
         Calc.register(sl);
-        c = Whitebox.invokeConstructor(Calc.class);
+        calc = Whitebox.invokeConstructor(Calc.class);
     }
 
     @Test
     public void getRandomIntBetweenTest() {
         for (int i = 0; i < 50; i++) {
-            int integer = c.getRandomIntBetween(1, 50);
+            int integer = calc.getRandomIntBetween(1, 50);
             assertThat(integer >= 1 || integer <= 50, is(true));
         }
     }
 
     @Test
     public void getRandomIntOneBetweenTest() {
-        int integer = c.getRandomIntBetween(1, 2);
+        int integer = calc.getRandomIntBetween(1, 2);
         assertThat(integer == 1 || integer == 2, is(true));
     }
 
     @Test(expected = AssertionError.class)
     public void getRandomIntBetweenTestWrong() {
-        int integer = c.getRandomIntBetween(5,2);
+        int integer = calc.getRandomIntBetween(5, 2);
     }
 
     @Test(expected = AssertionError.class)
     public void getRandomIntBetweenTestEqual() {
-        int integer = c.getRandomIntBetween(2,2);
+        int integer = calc.getRandomIntBetween(2, 2);
     }
 
     @Test
     public void randomDoubleTest() {
-        double d = c.getRandomDouble(1);
+        double d = calc.getRandomDouble(1);
         assertThat(d >= 0 || d <= 1, is(true));
     }
 
     @Test(expected = AssertionError.class)
     public void randomDoubleTestZero() {
-        double d = c.getRandomDouble(0);
+        double d = calc.getRandomDouble(0);
     }
 
     @Test(expected = AssertionError.class)
     public void randomDoubleTestNegative() {
-        double d = c.getRandomDouble(-42);
+        double d = calc.getRandomDouble(-42);
     }
 }
