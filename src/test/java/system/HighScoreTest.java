e0889f07b409189d34e7a024b66fdfc5706481da
Eric Cornelissen <ericornelissen@gmail.com>
2016-09-29 12:37:40 +0200

    Finished tests for HighScore class

--- src/test/java/system/HighScoreTest.java
+++ src/test/java/system/HighScoreTest.java
@@ -10,28 +10,58 @@ public class HighScoreTest {
     private HighScore score;
 
     // Test names
+    private String scoreNameDefault = "";
     private String scoreNameA = "Foo";
     private String scoreNameB = "Bar";
 
     // Test scores
-    private int scoreScore = 0;
+    private int scoreScoreDefault = 0;
+    private int scoreScoreA = 5;
+    private double scoreScoreB = 3.14d;
+    private int scoreScoreBExpected = 3;
+    private String scoreScoreC = "42";
+    private int scoreScoreCExpected = 42;
 
     @Test
-    public void testSpritesName1() {
-        score = new HighScore(scoreNameA, scoreScore);
+    public void testHighScoreName1() {
+        score = new HighScore(scoreNameA, scoreScoreDefault);
         assertThat(score.getName().equals(scoreNameA), is(true));
     }
 
     @Test
-    public void testSpritesName2() {
-        score = new HighScore(scoreNameB, scoreScore);
+    public void testHighScoreName2() {
+        score = new HighScore(scoreNameB, scoreScoreDefault);
         assertThat(score.getName().equals(scoreNameB), is(true));
     }
 
     @Test
-    public void testSpritesName3() {
-        score = new HighScore(scoreNameA, scoreScore);
+    public void testHighScoreNameNeg() {
+        score = new HighScore(scoreNameA, scoreScoreDefault);
         assertThat(score.getName().equals(scoreNameB), is(false));
+    }
+
+    @Test
+    public void testHighScoreScore1() {
+        score = new HighScore(scoreNameDefault, scoreScoreA);
+        assertThat(score.getScore() == scoreScoreA, is(true));
+    }
+
+    @Test
+    public void testHighScoreScore2() {
+        score = new HighScore(scoreNameDefault, scoreScoreB);
+        assertThat(score.getScore() == scoreScoreBExpected, is(true));
+    }
+
+    @Test
+    public void testHighScoreScore3() {
+        score = new HighScore(scoreNameDefault, scoreScoreC);
+        assertThat(score.getScore() == scoreScoreCExpected, is(true));
+    }
+
+    @Test
+    public void testHighScoreScoreNeg() {
+        score = new HighScore(scoreNameDefault, scoreScoreA);
+        assertThat(score.getScore() == scoreScoreA + 2, is(false));
     }
 
 }
