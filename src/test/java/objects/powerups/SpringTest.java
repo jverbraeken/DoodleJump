43d8a8a9fe713906aa51cf3403e238ff94c47f0c
Michael Tran <m.tran@student.tudelft.nl>
2016-09-30 13:09:12 +0200

    Added test for the animate method.

--- src/test/java/objects/powerups/SpringTest.java
+++ src/test/java/objects/powerups/SpringTest.java
@@ -10,6 +10,7 @@ import resources.sprites.ISpriteFactory;
 import resources.sprites.ISprite;
 import system.IServiceLocator;
 
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.verify;
 import static org.powermock.api.mockito.PowerMockito.mock;
 import static org.powermock.api.mockito.PowerMockito.when;
@@ -23,7 +24,7 @@ public class SpringTest {
     private IAudioManager audioManager;
     private IServiceLocator serviceLocator;
     private ISpriteFactory spriteFactory;
-    private ISprite sprite;
+    private ISprite sprite, newSprite;
     private IRenderer renderer;
     private Spring spring;
     private IDoodle doodle;
@@ -33,13 +34,18 @@ public class SpringTest {
         serviceLocator = mock(IServiceLocator.class);
         spriteFactory = mock(ISpriteFactory.class);
         sprite = mock(ISprite.class);
+        newSprite = mock(ISprite.class);
         audioManager = mock(IAudioManager.class);
         renderer = mock(IRenderer.class);
         doodle = mock(IDoodle.class);
         when(serviceLocator.getSpriteFactory()).thenReturn(spriteFactory);
         when(spriteFactory.getSpringSprite()).thenReturn(sprite);
+        when(spriteFactory.getSpringUsedSprite()).thenReturn(newSprite);
         when(serviceLocator.getAudioManager()).thenReturn(audioManager);
         when(serviceLocator.getRenderer()).thenReturn(renderer);
+
+        when(sprite.getHeight()).thenReturn(20);
+        when(newSprite.getHeight()).thenReturn(40);
     }
 
     @Test
@@ -61,6 +67,15 @@ public class SpringTest {
         spring = Whitebox.invokeConstructor(Spring.class, serviceLocator, 0, 0);
         spring.collidesWith(doodle);
         verify(doodle).collide(spring);
+    }
+
+    @Test
+    public void testAnimate() throws Exception {
+        double deviation = 0.001;
+        spring = Whitebox.invokeConstructor(Spring.class, serviceLocator, 50, 200);
+        Whitebox.invokeMethod(spring, "animate");
+        assertEquals(newSprite, spring.getSprite());
+        assertEquals(180, spring.getYPos(), deviation);
     }
 
 }
