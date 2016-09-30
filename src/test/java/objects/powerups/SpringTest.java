936ba51303abca237b9020ceb2a2500a11a6d592
Michael Tran <m.tran@student.tudelft.nl>
2016-09-30 12:44:59 +0200

    Added test class for the Spring class

---
+++ src/test/java/objects/powerups/SpringTest.java
@@ -0,0 +1,56 @@
+package objects.powerups;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.powermock.reflect.Whitebox;
+import rendering.IRenderer;
+import resources.audio.IAudioManager;
+import resources.sprites.ISpriteFactory;
+import resources.sprites.ISprite;
+import system.IServiceLocator;
+
+import static org.mockito.Mockito.verify;
+import static org.powermock.api.mockito.PowerMockito.mock;
+import static org.powermock.api.mockito.PowerMockito.when;
+
+
+/**
+ * Created by Michael on 9/30/2016.
+ */
+public class SpringTest {
+
+    private IAudioManager audioManager;
+    private IServiceLocator serviceLocator;
+    private ISpriteFactory spriteFactory;
+    private ISprite sprite;
+    private IRenderer renderer;
+    private Spring spring;
+
+    @Before
+    public void init() {
+        serviceLocator = mock(IServiceLocator.class);
+        spriteFactory = mock(ISpriteFactory.class);
+        sprite = mock(ISprite.class);
+        audioManager = mock(IAudioManager.class);
+        renderer = mock(IRenderer.class);
+        when(serviceLocator.getSpriteFactory()).thenReturn(spriteFactory);
+        when(spriteFactory.getSpringSprite()).thenReturn(sprite);
+        when(serviceLocator.getAudioManager()).thenReturn(audioManager);
+        when(serviceLocator.getRenderer()).thenReturn(renderer);
+    }
+
+    @Test
+    public void testPlaySound() throws Exception {
+        spring = Whitebox.invokeConstructor(Spring.class, serviceLocator, 0, 0);
+        Whitebox.invokeMethod(spring, "playSound");
+        verify(audioManager).playFeder();
+    }
+
+    @Test
+    public void testRenderer() throws Exception {
+        spring = Whitebox.invokeConstructor(Spring.class, serviceLocator, 0, 0);
+        spring.render();
+        verify(renderer).drawSprite(sprite, (int) spring.getXPos(), (int) spring.getYPos());
+    }
+
+}
