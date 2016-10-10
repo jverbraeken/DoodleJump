3c04c5c69ea83f849111ba65f862db8ba2ebec0d
Michael Tran <m.tran@student.tudelft.nl>
2016-10-10 09:07:28 +0200

    Set up for the test class.

--- src/test/java/resources/ResTest.java
+++ src/test/java/resources/ResTest.java
@@ -1,7 +1,24 @@
 package resources;
 
+import org.junit.Before;
+import org.powermock.reflect.Whitebox;
+import resources.sprites.ISprite;
+import system.IServiceLocator;
+import static org.powermock.api.mockito.PowerMockito.mock;
+
 /**
  * Created by Michael on 10/10/2016.
  */
 public class ResTest {
+
+    private IServiceLocator serviceLocator;
+    private Res res;
+    private ISprite sprite;
+
+    @Before
+    public void setUp() throws Exception {
+        serviceLocator = mock(IServiceLocator.class);
+        sprite = mock(ISprite.class);
+        res = Whitebox.invokeConstructor(Res.class);
+    }
 }
