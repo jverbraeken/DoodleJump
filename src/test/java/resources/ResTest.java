92c49f0147e956cfd73481c033b0c4024bfa5ad7
Michael Tran <m.tran@student.tudelft.nl>
2016-10-10 10:33:50 +0200

    test of getFilePath method (good weather).

--- src/test/java/resources/ResTest.java
+++ src/test/java/resources/ResTest.java
@@ -7,6 +7,11 @@ import org.junit.rules.ExpectedException;
 import org.powermock.reflect.Whitebox;
 import resources.sprites.ISprite;
 import system.IServiceLocator;
+
+import java.util.EnumMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
 import static org.powermock.api.mockito.PowerMockito.mock;
 
 /**
@@ -17,6 +22,11 @@ public class ResTest {
     private IServiceLocator serviceLocator;
     private Res res;
     private ISprite sprite;
+    private String filePath = "mockedSprite.jpg";
+    private Map<IRes.Sprites, String> mockedSprites = new EnumMap<>(IRes.Sprites.class);
+   // private enum MockedSprites {
+     //   mock
+    //}
 
     @Rule
     public ExpectedException thrown = ExpectedException.none();
@@ -32,5 +42,12 @@ public class ResTest {
     public void testRegisterNullInput() {
         thrown.expect(AssertionError.class);
         res.register(null);
+    }
+
+    @Test
+    public void testGetSpritePath() {
+        mockedSprites.put(IRes.Sprites.menu, filePath);
+        Whitebox.setInternalState(res, "sprites", mockedSprites);
+        assertEquals(filePath, res.getSpritePath(IRes.Sprites.menu));
     }
 }
