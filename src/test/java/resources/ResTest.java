4dd34d7270e413ec165b83cbd04dca14bacf8332
Michael Tran <m.tran@student.tudelft.nl>
2016-10-10 09:14:28 +0200

    Test of register method with input null.

--- src/test/java/resources/ResTest.java
+++ src/test/java/resources/ResTest.java
@@ -1,6 +1,9 @@
 package resources;
 
 import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.powermock.reflect.Whitebox;
 import resources.sprites.ISprite;
 import system.IServiceLocator;
@@ -15,10 +18,19 @@ public class ResTest {
     private Res res;
     private ISprite sprite;
 
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
     @Before
     public void setUp() throws Exception {
         serviceLocator = mock(IServiceLocator.class);
         sprite = mock(ISprite.class);
         res = Whitebox.invokeConstructor(Res.class);
+    }
+
+    @Test
+    public void testRegisterNullInput() {
+        thrown.expect(AssertionError.class);
+        res.register(null);
     }
 }
