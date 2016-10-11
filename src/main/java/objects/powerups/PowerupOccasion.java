b9002c479b3e9b5e63eae16ce31a30bf158e185d
Eric Cornelissen <ericornelissen@gmail.com>
2016-10-11 09:14:17 +0200

    Added missing Javadoc

--- src/main/java/objects/powerups/PowerupOccasion.java
+++ src/main/java/objects/powerups/PowerupOccasion.java
@@ -2,7 +2,14 @@ package objects.powerups;
 
 public enum PowerupOccasion {
 
+    /**
+     * For powerups that should have effect constantly.
+     */
     constant,
+
+    /**
+     * For powerups that should only have effect on collision.
+     */
     collision
 
 }
