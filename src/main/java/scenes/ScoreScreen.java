f0854a80c5b41b43beaa31fdca7f326f81ee8eab
Joost Verbraeken <j.verbraeken@hotmail.com>
2016-10-14 12:33:42 +0200

    Fix star import

--- src/main/java/scenes/ScoreScreen.java
+++ src/main/java/scenes/ScoreScreen.java
@@ -10,7 +10,7 @@ import resources.sprites.ISprite;
 import resources.sprites.ISpriteFactory;
 import system.IServiceLocator;
 
-import java.awt.*;
+import java.awt.Color;
 import java.util.List;
 
 /**
