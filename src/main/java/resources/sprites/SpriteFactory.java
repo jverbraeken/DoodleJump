1e80cfc0f22813e8040cd10f6be8df6c48d41d0c
Eric Cornelissen <ericornelissen@gmail.com>
2016-10-19 12:47:22 +0200

    Fixed failing tests

--- src/main/java/resources/sprites/SpriteFactory.java
+++ src/main/java/resources/sprites/SpriteFactory.java
@@ -39,7 +39,9 @@ public final class SpriteFactory implements ISpriteFactory {
      * @param sL The IServiceLocator to which the class should offer its functionality
      */
     public static void register(final IServiceLocator sL) {
-        assert sL != null;
+        if (sL == null) {
+            throw new IllegalArgumentException("The service locator cannot be null");
+        }
         SpriteFactory.serviceLocator = sL;
         SpriteFactory.serviceLocator.provide(new SpriteFactory());
     }
