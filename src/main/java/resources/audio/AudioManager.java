2c64957f3151964ad10a2f046cbf6cd33699584b
Eric Cornelissen <ericornelissen@gmail.com>
2016-11-04 15:48:59 +0100

    Preload all sounds

--- src/main/java/resources/audio/AudioManager.java
+++ src/main/java/resources/audio/AudioManager.java
@@ -53,12 +53,49 @@ public final class AudioManager implements IAudioManager {
      */
     @Override
     public void preload() {
+        this.loadClip(Sounds.BIJELI);
+        this.loadClip(Sounds.BLIZZARD);
+        this.loadClip(Sounds.BUBBLES1);
+        this.loadClip(Sounds.BUBBLES2);
+        this.loadClip(Sounds.CHILL);
+        this.loadClip(Sounds.COLLECT);
+        this.loadClip(Sounds.CRNARUPA);
+        this.loadClip(Sounds.EGGMONSTERHIT);
+        this.loadClip(Sounds.EXPLODING_PLATFORM);
+        this.loadClip(Sounds.EXPLODING_PLATFORM2);
         this.loadClip(Sounds.FEDER);
+        this.loadClip(Sounds.JETPACK);
         this.loadClip(Sounds.JUMP);
+        this.loadClip(Sounds.JUMP_ON_MONSTER);
         this.loadClip(Sounds.LOMISE);
+        this.loadClip(Sounds.MATCH_SOUND);
+        this.loadClip(Sounds.MONSTER_CRASH);
+        this.loadClip(Sounds.MONSTER_BLIZU);
+        this.loadClip(Sounds.MONSTER_POGODAK);
+        this.loadClip(Sounds.OOGAPUCANJE);
+        this.loadClip(Sounds.OOGAPUCANJE2);
+        this.loadClip(Sounds.PADA);
+        this.loadClip(Sounds.PROPELLER);
+        this.loadClip(Sounds.PUCANJE);
+        this.loadClip(Sounds.PUCANJE2);
+        this.loadClip(Sounds.RAIN);
+        this.loadClip(Sounds.ROCKET);
+        this.loadClip(Sounds.SNOWBALL_MONSTER_HIT);
+        this.loadClip(Sounds.SNOWBALL_THROW);
+        this.loadClip(Sounds.SNOWBALL_THROW2);
+        this.loadClip(Sounds.SOCCER_MONSTER_CRASH);
+        this.loadClip(Sounds.SOCCER_MONSTER_HIT);
+        this.loadClip(Sounds.SPRING_SHOES);
         this.loadClip(Sounds.START);
+        this.loadClip(Sounds.THUNDER);
         this.loadClip(Sounds.THEME_SONG);
         this.loadClip(Sounds.TRAMPOLINE);
+        this.loadClip(Sounds.UFO);
+        this.loadClip(Sounds.UFO_POGODAK);
+        this.loadClip(Sounds.UNDERWATER_SHOOT);
+        this.loadClip(Sounds.UNDERWATER_SHOOT2);
+        this.loadClip(Sounds.USAUGATEUFO);
+        this.loadClip(Sounds.WIN);
     }
 
     /**
